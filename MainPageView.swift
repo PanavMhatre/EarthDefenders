1b9db8a85c9cfb33b2b6f7b89ca2e40a3718c8f6
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-18 18:03:07 -0500

    Update MainPageView.swift

--- MainPageView.swift
+++ MainPageView.swift
@@ -3,6 +3,41 @@ import SwiftUI
 struct MainPage: View {
     @Environment(\.presentationMode) var presentationMode
 
+    var buttonAction: some View{
+        GeometryReader{geo in
+            HStack{
+                NavigationLink(destination: HomeView()){
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 75, height: 75)
+                        .shadow(radius: 10)
+                        .overlay(
+                            Image(systemName: "info.circle.fill")
+                                .font(.system(size: 50))
+                                .foregroundColor(Color.blue)
+                        ).padding(.bottom, 50)
+                }
+                
+                NavigationLink(destination: ContentView()) {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.brown)
+                        .frame(width: 75, height: 75)
+                        .shadow(radius: 10)
+                        .overlay(
+                            Image(systemName: "house.fill")
+                                .font(.system(size: 40))
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        )
+                }
+                .padding(.bottom, 50)
+
+                
+            }.position(x: geo.size.width/2,y: geo.size.height/1.03)
+        }
+    }
+
+
     var countries:some View{
         GeometryReader{geo in
             ZStack{
