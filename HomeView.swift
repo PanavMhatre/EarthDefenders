1c247077331eac8b3775f42f425cf683fd2c3bed
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-18 13:31:46 -0500

    Update HomeView.swift

--- HomeView.swift
+++ HomeView.swift
@@ -47,6 +47,75 @@ struct HomeView: View {
             .edgesIgnoringSafeArea(.all)
     }
     
+    var backgroundStory1: some View{
+        VStack{
+            HStack{
+                RoundedRectangle(cornerRadius: 10) // Card shape
+                    .fill(Color.cyan) // Blue background color
+                    .frame(width: 400, height: 200)
+                    .overlay(
+                        Image("worldBackground")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(20).frame(width: 400) // Padding around the image inside the card
+                    )
+                
+            }
+            
+            Text(information.infoIntroduction[0])
+                .font(.title)
+                .fontWeight(.semibold)
+                .baselineOffset(4)
+                .kerning(1.5)
+                .multilineTextAlignment(.leading)
+                .foregroundColor(.white)
+                .padding()
+                .padding(.bottom, 30)
+            HStack{
+                Spacer()
+                HStack{
+                    Button(action: {
+                        isBackgroundStory1.toggle()
+                        isBackgroundStory2.toggle()
+                    }, label:{
+                        Image(systemName: "arrow.right")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .bold()
+                            .padding(.horizontal)
+                            .padding()
+                            .padding(.vertical)
+                            .background(
+                                Color.black.opacity(0.5)
+                            )
+                            .cornerRadius(25)
+                    })
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+            .overlay(
+                HStack{
+                    Image("boy1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 150)
+                }
+                ,alignment: .bottomLeading
+            )
+        }
+        .padding(.top, 50)
+        .padding()
+        .background(
+            Color.brown
+        )
+        .cornerRadius(25)
+        .padding(16)
+        .padding(.horizontal, 60)
+        .transition(AnyTransition.scale.animation(.easeInOut))
+    }
+
+    
 
 
 struct IntroductionView_Previews: PreviewProvider {
