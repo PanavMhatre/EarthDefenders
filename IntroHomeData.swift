bb7ea13ac5a3f44b2522bfa876dfcf6ba3a031ab
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-17 22:29:30 -0500

    Create IntroHomeData.swift

---
+++ IntroHomeData.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Info {
+    let infoIntroduction: [String]
+}
+let information = Info(
+    infoIntroduction: [
+    "Around the world we share a beatiful world filled with various plants and animals. \n\nIt is home to over 7,078 mammal species, 15,125 freshwater fish species, 32,321 marine fish species, 10,721 bird species, and 10,793 reptile species.",
+    "Out of the thousands of species, there are 292 species of endemic mammals, 2,500 species of fish, 515 species of reptiles, and 430 species of birds in world. Moreover we have seen a 18 percent loss of habitat. \n\nSome famous endemic species is the American Alligator, Giant Panda, and Platypus.",
+    "But many of endemic animals and climates are threatened with extinction due to the loss of habitat and environmental damage. \n\nAs someone who loves to travel the world it is really sad to diminishing beauty of this world. This is an issue I am passionate about and is really important to know about!"
+    
+    ]
+)
