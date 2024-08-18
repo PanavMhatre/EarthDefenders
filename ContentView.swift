7a36abaf1d986c69e51410d953b5e40c42c0fbde
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-17 19:15:15 -0500

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -7,6 +7,55 @@ struct ContentView: View {
     @State private var cloud3Offset: CGFloat = -250
     @State private var cloud2Offset: CGFloat = UIScreen.main.bounds.width + 150
     @State private var cloud4Offset: CGFloat = UIScreen.main.bounds.width + 150
+
+    var cloudsView: some View{
+        ZStack{
+            Image("cloud")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250)
+                .position(x: 0,y: 150)
+                .offset(x: cloud1Offset)
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 15).repeatForever(autoreverses: false)) {
+                        self.cloud1Offset = UIScreen.main.bounds.width + 150
+                    }
+                }
+            Image("cloud")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150)
+                .position(x: 0,y: 400)
+                .offset(x: cloud2Offset)
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 20).repeatForever(autoreverses: false)) {
+                        self.cloud2Offset = -100
+                    }
+                }
+            Image("cloud")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+                .position(x: 0,y: 900)
+                .offset(x: cloud4Offset)
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 17).repeatForever(autoreverses: false)) {
+                        self.cloud4Offset = -100
+                    }
+                }
+            Image("cloud")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150)
+                .position(x: 0,y: 700)
+                .offset(x: cloud3Offset)
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 10).repeatForever(autoreverses: false)) {
+                        self.cloud3Offset = UIScreen.main.bounds.width + 150
+                    }
+                }
+        }
+    }
     
     
     
